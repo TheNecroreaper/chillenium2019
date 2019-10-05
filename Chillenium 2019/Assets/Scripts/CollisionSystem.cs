686aae1ab9efa6be4cd0abbea29e86e68e5d6b3f
Alex Wu <alexwu2005@gmail.com>
2019-10-05 01:13:25 -0500

    starting files

---
+++ Chillenium 2019/Assets/Scripts/CollisionSystem.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSystem : MonoBehaviour
+{
+
+    //Main Object collision data
+    private Renderer render;
+    private Rigidbody rb;
+    private Vector3 objectBoundMax;
+    private Vector3 objectBoundMin;
+    private const float OFFSET = 0.5f;
+    private Vector3 OFFSET_VECTOR;
+
+    //Relationship data
+    private bool withinXBound;
+    private bool withinYBound;
+
+    //Collider Data
+    private string colliderTag;
+    private Vector3 colliderMaxBounds;
+    private Vector3 colliderMinBounds;
+
+    //Behavior enablers
+    public bool grounded;
+
+    //Collider Dictionary
+    private Dictionary<string, Collider> touching;
+
+    // Use this for initialization
+    void Start()
+    {
+        render = GetComponent<Renderer>();
+        rb = GetComponent<Rigidbody>();
+        OFFSET_VECTOR = new Vector3(OFFSET, OFFSET);
+        touching = new Dictionary<string, Collider>();
+    }
+
+    //Updates by checking if still touching with colliders in "touching" dictionary
+    void Update()
+    {
+        //Updates offset bound every frame
+        Vector3 offsetExtent = render.bounds.extents + OFFSET_VECTOR;
+        Bounds touchBound = new Bounds(render.bounds.center, 2 * offsetExtent);
+
+        //Get all keys in a list
+        List<string> keyList = new List<string>(touching.Keys);
+
+        //Checks the status of touching colliders. If collider destroyed or not touching anymore, remove it
+        foreach (string key in keyList)
+        {
+            Collider touchedObj = touching[key];
+
+            if (touchedObj == null || (touchedObj.tag != "Platform") || !touchedObj.bounds.Intersects(touchBound))
+                touching.Remove(key);
+        }
+
+        //Updates condition variables as needed depending if its touching assigned obj
+        grounded = touching.ContainsKey("ground");
+
+        //Checks for corner bug when y velocity == 0 and y isn't fixed
+        bool fixedY = (rb.constraints & RigidbodyConstraints.FreezePositionY) != RigidbodyConstraints.None;
+        if (!grounded && rb.velocity.y == 0 && !fixedY)
+            StartCoroutine(recalibrate());
+
+    }
+
+    //Constants for recalibarion
+    private const float RECALIBRATE_HOP = 50f;
+    private const int NUM_FRAMES = 3;
+
+    //Re-calibrates collision system if bug exists by allowing the player to make a small hop if delta-Y is 0
+    private IEnumerator recalibrate()
+    {
+        //Gets delta y
+        float originalY = GetComponent<Transform>().position.y;
+
+        //Allows fixed number of frames before checking the next y
+        for (int i = 0; i < NUM_FRAMES; i++)
+            yield return 0;
+
+        float newY = GetComponent<Transform>().position.y;
+
+        //Checks if there's no change in y
+        if (newY == originalY && !grounded)
+            GetComponent<Rigidbody>().AddForce(Vector3.up * RECALIBRATE_HOP);
+    }
+
+
+    //checks enter collision
+    void OnCollisionEnter(Collision collision)
+    {
+        setColliderData(collision);
+
+        //If player is on a platform, grounded = true. Grounded is automatically false when object jumps
+        if (withinXBound && (colliderTag == "Platform") && !grounded)
+            touching.Add("ground", collision.collider);
+    }
+
+    //Sets all collision data after every collision
+    private void setColliderData(Collision collision)
+    {
+        Collider collider = collision.collider;
+
+        //Set Collider Variables
+        colliderTag = collider.tag;
+        colliderMaxBounds = collider.bounds.max;
+        colliderMinBounds = collider.bounds.min;
+
+        //Set Main Object variables
+        objectBoundMax = render.bounds.max;
+        objectBoundMin = render.bounds.min;
+
+        //Set relationship Variables: withinXBound
+        withinXBound = colliderMaxBounds.x > objectBoundMin.x + OFFSET && colliderMinBounds.x < objectBoundMax.x - OFFSET;
+        withinYBound = colliderMaxBounds.y > objectBoundMin.y + OFFSET && colliderMinBounds.y < objectBoundMax.y - OFFSET;
+
+    }
+}
---
+++ Chillenium 2019/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    //Reference Variables
+    private CollisionSystem playerCollision;
+    private Transform player;
+    private Vector3 spawnPoint;
+    //Constants and variables for horizontal movement
+    private const float GROUNDED_MOVEMENT = 0.15f;
+    private const float AIR_MOVEMENT = 0.1f;
+    private float moveSpeed;
+
+    //Constant for jumping
+    private const float JUMP_FORCE = 200f;
+
+    // Use this for initialization
+    void Start()
+    {
+        setVars();
+    }
+
+    public void setVars()
+    {
+        playerCollision = GetComponent<CollisionSystem>();
+        player = GetComponent<Transform>();
+        spawnPoint = player.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Set status
+        if (playerCollision.grounded)
+        {
+            moveSpeed = GROUNDED_MOVEMENT;
+        }
+        else
+        {
+            moveSpeed = AIR_MOVEMENT;
+        }
+
+        //Controls
+        if (Input.GetKey("d") && !playerCollision.rightWalled)              //Move right     
+            player.position += Vector3.right * moveSpeed;
+
+        if (Input.GetKey("a") && !playerCollision.leftWalled)               //Move left
+            player.position += Vector3.left * moveSpeed;
+
+        if (Input.GetKeyDown("space") && playerCollision.grounded)          //Jump
+            GetComponent<Rigidbody>().AddForce(Vector3.up * JUMP_FORCE);
+
+    }
+}
